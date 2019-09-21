40d69099a020dad88ee270a1a081db8d1e4913ce
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-09-20 19:52:16 -0700

    finished basic routing and added mongoose schema

---
+++ models/book.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//this is the form that the schema in mongoose takes
+const bookSchema = new Schema({
+  title: { type: String, required: true },
+  authors: [{ type: String, required: true }],
+  synopsis: String,
+  date: { type: Date, default: Date.now },
+  thumbnail: { type: String },
+  link: String
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
