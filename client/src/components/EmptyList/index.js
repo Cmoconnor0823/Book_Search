b991804c7cb37818f0c817ca38dc6fdbc8d4f713
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-09-25 16:11:23 -0700

    moving away from hooks and back to a set state to try and make post to db work

---
+++ client/src/components/EmptyList/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function EmptyList(){
+    return(
+        <div>
+            <h2>There are no Books Saved to display! Enter a search find a book to read!</h2>
+        </div>
+    )
+}
--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,96 +1,110 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import axios from "axios";
 import AddBookBtn from "../../components/AddBookBtn";
-//import { Row, Col } from "../../components/Grid";
-//import { BookList, BookListItem } from "../../components/BookList";
-//import EmptyList from "../../components/EmptyList";
+import { Row, Col } from "../../components/Grid";
+import { BookList, BookListItem } from "../../components/BookList";
+import EmptyList from "../../components/EmptyList";
 
 
 
-const Search = () => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [books, setBooks] = useState({ items: [] });
-    const onInputChange = (e) => {
-        setSearchTerm(e.target.value);
+class Search extends Component {
+    state = {
+      searchRes: [],
+      query: "",
+      books: []
+    };
+  
+    displayRes = data => {
+      this.setState({ books: data.items });
+    };
+  
+    searchGBooks = () => {
+      let url = `https://www.googleapis.com/books/v1/volumes?q=${
+        this.state.query
+      }`;
+      axios
+        .get(url)
+        .then(res => {
+          //console.log(res);
+          this.displayRes(res.data);
+        })
+        .catch(err => console.log(err));
+    };
+  
+    handleInput = event => {
+      const { name, value } = event.target;
+  
+      this.setState({
+        [name]: value
+      });
+      //console.log("Query", this.state.query);
+    };
+  
+  
+    
+  
+    render() {
+      return (
+        <Row>
+          <Col size="md-12">
+          <div>
+            <input id="bookQ" className="form-control form-control-lg" autoComplete="off" type="text" name="query" onChange={this.handleInput} />
+            <button  type="submit" onClick={this.searchGBooks} >
+              Search for Books
+            </button>
+                     
+  
+            {(this.state.books && this.state.books.length > 0) ? 
+            <BookList>
+            {this.state.books.map(book => {
+              return (
+                <div>
+                <BookListItem
+                key={book.id} 
+                authors={book.volumeInfo.authors ? book.volumeInfo.authors : ["No Author Available"]}
+                title={book.volumeInfo.title}
+                synopsis={book.volumeInfo.description ? 
+                  book.volumeInfo.description : "No Description Available"}
+                link={book.volumeInfo.infoLink}
+                thumbnail={book.volumeInfo.imageLinks.thumbnail ? 
+                  book.volumeInfo.imageLinks.thumbnail : "#"}
+                />
+  
+                <AddBookBtn
+                authors={book.volumeInfo.authors ? book.volumeInfo.authors : ["No Author Available"]}
+                title={book.volumeInfo.title}
+                synopsis={book.volumeInfo.description ? 
+                  book.volumeInfo.description : "No Description Available"}
+                link={book.volumeInfo.infoLink}
+                thumbnail={book.volumeInfo.imageLinks.thumbnail ? 
+                  book.volumeInfo.imageLinks.thumbnail : "#"}
+                
+                />
+                </div>
+              )
+            })}
+            </BookList>
+            : 
+            <EmptyList/>         
+            }
+            
+          </div>
+          </Col>
+        </Row>
+      );
     }
-
-    let API_URL = `https://www.googleapis.com/books/v1/volumes`;
-
-    const fetchBooks = async () => {
-        // Ajax call to API using Axios
-        const result = await axios.get(`${API_URL}?q=${searchTerm}`);
-        // Books result
-        console.log(result.data);
-        setBooks(result.data);
-    }
-
-    const onSubmitHandler = (e) => {
-        // Prevent browser refreshing after form submission
-        e.preventDefault();
-        // Call fetch books async function
-        fetchBooks();
-    }
-
-    const bookAuthors = (authors) => {
-        if (authors.length <= 2) {
-            authors = authors.join(' and ')
-        }
-        else if (authors.length > 2) {
-            let lastAuthor = ' and ' + authors.slice(-1);
-            authors.pop();
-            authors = authors.join(', ');
-            authors += lastAuthor;
-        };
-        return authors;
-    }
-    return (
-        <section>
-            <form onSubmit={onSubmitHandler}>
-                <label>
-                    <span>Search for books</span>
-                    <input
-                        type="search"
-                        placeholder="microservice, restful design, etc.,"
-                        value={searchTerm}
-                        onChange={onInputChange}
-                    />
-                    <button type="submit">Search</button>
-                </label>
-            </form>
-            <ul>
-                {
-                    books.items.map((book, index) => {
-                        return (
-                            <li key={book.id}>
-                                <div>
-                                    <img alt={`${book.volumeInfo.title} book`} src={`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`} />
-                                    <div>
-                                        <h3>{book.volumeInfo.title}</h3>
-                                        <p>{bookAuthors(book.volumeInfo.authors)}</p>
-                                        <p>{(book.volumeInfo.description ?
-                                            book.volumeInfo.description : "No Description Available")}</p>
-                                        <p>{book.volumeInfo.publishedDate}</p>
-                                    </div>
-                                </div>
-                                <hr />
-                                <AddBookBtn
-                                    authors={book.volumeInfo.authors ? book.volumeInfo.authors : ["No Author Available"]}
-                                    title={book.volumeInfo.title}
-                                    synopsis={book.volumeInfo.description ?
-                                        book.volumeInfo.description : "No Description Available"}
-                                    link={book.volumeInfo.infoLink}
-                                    thumbnail={book.volumeInfo.imageLinks.thumbnail ?
-                                        book.volumeInfo.imageLinks.thumbnail : "#"}
-
-                                />
-                            </li>
-                        );
-                    })
-                }
-            </ul>
-        </section>
-    );
-
-}
+  }
 
 export default Search;
+        // const bookAuthors = (authors) => {
+        //     if (authors.length <= 2) {
+        //         authors = authors.join(' and ')
+        //     }
+        //     else if (authors.length > 2) {
+        //         let lastAuthor = ' and ' + authors.slice(-1);
+        //         authors.pop();
+        //         authors = authors.join(', ');
+        //         authors += lastAuthor;
+        //     };
+        //     return authors;
+        // }
